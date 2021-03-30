4e115e53e55736b6918d52689777cc01554c67f5
Tristan Chambers <tmchambers@nacdl.org>
2021-03-30 13:54:50 -0400

    Update settings file to point to Azure settings

--- fdp/settings.py
+++ fdp/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 #: To configure hosting in a local development environment, remove the comment from the below import statement.
 #: This should not be used in a production environment.
-from .configuration.local.local_settings import *
-
+# from .configuration.local.local_settings import *
+from .configuration.azure.azure_settings import *
 
 #: To configure hosting in a Microsoft Azure environment, remove the comment from the below import statement.
 # from .configuration.azure.azure_settings import *
