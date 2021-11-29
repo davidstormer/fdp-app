9250b40af6f52ad936ca627da8dc07e305e73a40
freedobject <80773529+freedobject@users.noreply.github.com>
2021-11-29 18:26:45 -0500

    Add new automated tests for FDAB-266
    Add automated tests in fdpuser app to check for federated login for local, Azure and Azure-only configurations.

--- inheritable/models.py
+++ inheritable/models.py
@@ -3151,8 +3151,8 @@ class AbstractConfiguration(models.Model):
                         o['url_pattern_name'],
                         args=[locate(a) for a in o['url_pattern_args']]
                     ) if o['url_pattern_args'] else reverse(o['url_pattern_name']),
-                    'css': o['css'],
-                    'css_hover': o['css_hover']
+                    'css': o.get('css', {}),
+                    'css_hover': o.get('css_hover', {})
                 } for o in login_options
             ]
         else:
