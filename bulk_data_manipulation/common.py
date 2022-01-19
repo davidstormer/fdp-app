a00de1d4ddac007e1efe40980fbb614b653fed3f
Tristan Chambers <tmchambers@nacdl.org>
2022-01-19 12:43:21 -0500

    In the middle of refactoring common pattern of processing CSV with atomic rollback behavior. NEXT: Need to write tests for CsvBulkCommand.csv_bulk_action()!!!

--- bulk_data_manipulation/common.py
+++ bulk_data_manipulation/common.py
@@ -1,3 +1,9 @@
+import os
+from importlib import import_module
+
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
 from bulk.models import BulkImport
 from uuid import uuid4
 
@@ -55,3 +61,53 @@ def get_record_from_external_id(model, external_id):
                             f" {external_id}")
 
     return record
+
+def get_model_from_import_string(import_string: str) -> object:
+    """Assumes that import string follows the pattern '[app_name].models.[model_name]'"""
+    model_import_elements = import_string.split('.')
+    app_name = model_import_elements[0]
+    if model_import_elements[1] != 'models':
+        raise Exception(f"Model import string must follow the pattern '[app_name].models.[model_name]' but "
+                        f"{model_import_elements[1]} != 'models'")
+    model_name = model_import_elements[2]
+    model = getattr(getattr(import_module(app_name), 'models'), model_name)
+    return model
+
+
+class CsvBulkCommand(BaseCommand):
+    """Customized BaseCommand for including additional helper methods shared by FDP management command tools.
+    Further reading: https://docs.djangoproject.com/en/3.2/howto/custom-management-commands/
+    """
+
+    def handle(self, *args, **options):
+        raise NotImplementedError('subclasses of CsvBulkCommand must provide a handle() method. See '
+                                  'https://docs.djangoproject.com/en/3.2/howto/custom-management-commands/')
+
+    def csv_bulk_action(self, options, callback_func, **kwargs):
+        if os.stat(options['input_file']).st_size < 1:
+            self.stdout.write(self.style.ERROR(f"WARNING! {options['input_file']} is an empty file. Doing nothing. "
+                                               f"Quitting..."))
+        model = get_model_from_import_string(options['model'])
+        self.stdout.write(self.style.WARNING(
+            f"Delete {model} records..."
+        ))
+        with open(options['input_file'], 'r') as csv_fd:
+            try:
+                with transaction.atomic():
+                    errors = []
+                    for external_id in csv_fd:
+                        external_id = external_id.strip()
+                        try:
+                            callback_func(model, external_id, **kwargs)
+                        except Exception as e:
+                            errors.append(e)
+
+                    if len(errors) > 0:
+                        self.stdout.write(self.style.ERROR("Errors encountered! Undoing..."))
+                        for error in errors:
+                            self.stdout.write(self.style.ERROR(' '.join(error.args)))
+                        self.stdout.write(self.style.ERROR("Undoing..."))
+
+                        raise ImportErrors
+            except ImportErrors:
+                self.stdout.write(self.style.ERROR("Quitting..."))
