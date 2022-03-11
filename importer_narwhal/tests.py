f3c7ec16342f6ba1f5c198b71a5636d7eb2a8748
Tristan Chambers <tristan.chambers@gmail.com>
2022-03-10 22:20:20 -0500

    Test django-import-export based app with custom boolean widget

---
+++ importer_narwhal/tests.py
@@ -0,0 +1,27 @@
+import tablib
+from django.test import TestCase
+from .narwhal import BooleanWidgetValidated
+from core.models import Person
+
+
+class NarwhalTestCase(TestCase):
+
+    def test_booleanwidgetvalidated(self):
+        with self.subTest(value='INVALID'):
+            with self.assertRaises(ValueError):
+                BooleanWidgetValidated().clean(value='INVALID')
+        with self.subTest(value='TRUE'):
+            self.assertTrue(
+                BooleanWidgetValidated().clean(value='TRUE'),
+                msg='Unexpected validation error for "TRUE"'
+            )
+        with self.subTest(value='FALSE'):
+            self.assertFalse(
+                BooleanWidgetValidated().clean(value='FALSE'),
+                msg='Unexpected validation error for "FALSE"'
+            )
+        with self.subTest(value=''):
+            self.assertIsNone(
+                BooleanWidgetValidated().clean(value=''),
+                msg='Unexpected validation error for blank value'
+            )
