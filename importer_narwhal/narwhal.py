d658ba1bd24d549170bf14ce9dfa14f31bcce2fb
Tristan Chambers <tristan.chambers@gmail.com>
2022-03-11 06:38:48 -0500

    Refactor

--- importer_narwhal/narwhal.py
+++ importer_narwhal/narwhal.py
@@ -1,56 +1,44 @@
-import tablib
-from import_export import resources, widgets
+from import_export import resources
 from import_export.resources import ModelResource
-
-from core.models import Person
+from importer_narwhal.widgets import BooleanWidgetValidated
 from wholesale.models import ModelHelper
+
+# The available list of models to import to
+# in the interface is based on this.
+MODEL_ALLOW_LIST = [
+    'Person',
+    'Content',
+    'PersonRelationship',
+    'PersonIdentifier',
+]
 
 
 class FdpModelResource(ModelResource):
+    """Customized django-import-export ModelResource
+    """
     pass
 
 
-class BooleanWidgetValidated(widgets.BooleanWidget):
-    # Raise an error when no suitable value found.
-    # The stock boolean widget doesn't do this!
-    # Also support 'checked' for True
-    def render(self, value, obj=None):
-        if value is None:
-            return ""
-        return 'TRUE' if value else 'FALSE'
-
-    def clean(self, value, row=None, *args, **kwargs):
-        value = value.lower()
-        if value in ["", None, "null", "none"]:
-            return None
-        elif value in ["1", 1, True, "true", 'checked']:
-            return True
-        elif value in ["0", 0, False, "false"]:
-            return False
-        else:
-            raise ValueError("Enter a valid boolean value.")
-
-
-# Use our custom boolean widget instead
+# Some of the stock widgets don't meet our needs
+# Override them with our custom versions
 FdpModelResource.WIDGETS_MAP['BooleanField'] = \
     BooleanWidgetValidated
 
-PersonResource = resources. \
-    modelresource_factory(
-    model=Person, resource_class=FdpModelResource)
 
-models_to_make_resources_for = [
-    'Person',
-    'Incident',
-]
+# We'll need a mother list of models and their corresponding
+# django-import-export resource.
+def compile_resources():
+    import_export_resources = {}
 
-import_export_resources = {}
+    for model_name in MODEL_ALLOW_LIST:
+        app_name = ModelHelper.get_app_name(model=model_name)
+        model_class = ModelHelper.get_model_class(app_name=app_name, model_name=model_name)
 
-for model_name in models_to_make_resources_for:
-    app_name = ModelHelper.get_app_name(model=model_name)
-    model_class = ModelHelper.get_model_class(app_name=app_name, model_name=model_name)
+        resource = resources. \
+            modelresource_factory(
+                model=model_class, resource_class=FdpModelResource)
+        import_export_resources[model_name] = resource
+    return import_export_resources
 
-    resource = resources. \
-        modelresource_factory(
-            model=model_class, resource_class=FdpModelResource)
-    import_export_resources[model_name] = resource
+
+resource_model_mapping = compile_resources()
