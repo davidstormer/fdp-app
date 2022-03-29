22049318ee7a217a4ac4707b511b8ef495de9c94
Tristan Chambers <tmchambers@nacdl.org>
2022-03-29 14:47:32 -0400

    Add test for "get or create" bulk edit scenario on Traits (m2m)

--- importer_narwhal/tests_export.py
+++ importer_narwhal/tests_export.py
@@ -1,4 +1,5 @@
 import csv
+from unittest import skip
 
 from django.test import TestCase
 from django.core.management import call_command
@@ -310,3 +311,80 @@ class NarwhalExportCommand(TestCase):
                     'miscellanist',
                     file_contents
                 )
+
+    # TODO: "get or create" logic on m2ms
+    # https://stackoverflow.com/questions/32369984/django-import-export-new-values-in-foriegn-key-model
+    @skip
+    def test_bulk_edit_scenario_m2m_traits(self):
+        # Given there are existing records in the system,
+        # some with "to_edit" in their name
+        existing_records = []
+        for i in range(5):
+            existing_records.append(import_record_with_extid(Person, {
+                'name': f'Person record {uuid4()}',
+                'traits': [Trait.objects.create(name=f'Test Trait {uuid4()}')],
+            }))
+        for i in range(5):
+            existing_records.append(import_record_with_extid(Person, {
+                'name': f'Person record to_edit {uuid4()}',
+                'traits': [Trait.objects.create(name=f'Test Trait {uuid4()}')],
+            }))
+
+        # Belt, _and_ suspenders...?
+        # self.assertEqual(
+        #     5,
+        #     Person.objects.filter(name__icontains='to_edit').count()
+        # )
+
+        # When I do an export, and edit the cells of the CSV, changing "to_edit" to "edited"
+        # and then import the CSV.
+        def search_replace(filename, search_for, replace_with):
+            # Read in the file
+            with open(filename, 'r') as file:
+                filedata = file.read()
+
+            # Replace the target string
+            filedata = filedata.replace(search_for, replace_with)
+
+            # Write the file out again
+            with open(filename, 'w') as file:
+                file.write(filedata)
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            bulk_edit_sheet_fn = temp_dir + '/bulk_edit_sheet.csv'
+            call_command('narwhal_export', 'Person', bulk_edit_sheet_fn)
+
+            search_replace(bulk_edit_sheet_fn, 'to_edit', 'edited')
+
+            command_output = StringIO()
+            call_command('narwhal_import', 'Person', bulk_edit_sheet_fn, stdout=command_output)
+
+        # Then the records should be updated with the edits
+        self.assertEqual(
+            0,
+            Person.objects.filter(name__icontains='to_edit').count(),
+            msg="Records with 'to_edit' in their name still found in the system!"
+        )
+        self.assertEqual(
+            5,
+            Person.objects.filter(name__icontains='edited').count()
+        )
+        # And Then the history logs should show five updates and accompanying diffs
+        history_command_output_stream = StringIO()
+        batch_number = ImportBatch.objects.last().pk
+        call_command('narwhal_import_history', batch_number, stdout=history_command_output_stream)
+        history_command_output = history_command_output_stream.getvalue()
+
+        self.assertEqual(
+            5,
+            history_command_output.count("update")
+        )
+        self.assertEqual(
+            5,
+            history_command_output.count("<del>to_</del><span>edit</span><ins>ed</ins>")
+        )
+        # And Then the history logs should not show edits to records that had no changes in the CSV
+        self.assertEqual(
+            5,
+            history_command_output.count("skip")
+        )
