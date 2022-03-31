68f7b183ace4723b4083c1747719d1ad819a9feb
Tristan Chambers <tmchambers@nacdl.org>
2022-03-31 17:12:20 -0400

    Test print out details and response

--- fdpuser/auth_pipeline.py
+++ fdpuser/auth_pipeline.py
@@ -6,5 +6,6 @@ logger = logging.getLogger(__name__)
 
 def auth_allowed(backend, details, response, *args, **kwargs):
     if not backend.auth_allowed(response, details):
-        logger.error("scammonyroot")
+        logger.error(f"DETAILS: {details}")
+        logger.error(f"RESPONSE: {response}")
         raise AuthForbidden(backend)
