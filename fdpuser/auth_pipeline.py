3d4a770ad185e349f15aba3bae650c9e42e4c3e6
Tristan Chambers <tmchambers@nacdl.org>
2022-04-01 06:39:11 -0400

    Nice error

--- fdpuser/auth_pipeline.py
+++ fdpuser/auth_pipeline.py
@@ -6,6 +6,7 @@ logger = logging.getLogger(__name__)
 
 def auth_allowed(backend, details, response, *args, **kwargs):
     if not backend.auth_allowed(response, details):
-        logger.error(f"DETAILS: {details}")
-        logger.error(f"RESPONSE: {response}")
+        logger.error(f"AUDIT | Failed login: email \"{details.get('email')}\" domain not found in "
+                     f"FDP_SOCIAL_AUTH_AZUREAD_TENANT_OAUTH2_WHITELISTED_DOMAINS. Response UPN: "
+                     f"\"{response.get('upn')}\"")
         raise AuthForbidden(backend)
