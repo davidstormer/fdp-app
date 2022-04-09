0349c88f06cd883d5716bb051890ad69de8f2c9a
Tristan Chambers <tristan.chambers@gmail.com>
2022-04-09 07:17:29 -0400

    Add batch setup page (untested)

--- importer_narwhal/urls.py
+++ importer_narwhal/urls.py
@@ -6,4 +6,5 @@ app_name = 'importer_narwhal'
 
 urlpatterns = [
     path('batch/<int:pk>', views.ImportBatchDetailView.as_view(), name="batch"),
+    path('batch/set-up', views.ImportBatchCreateView.as_view(), name="batch-create"),
 ]
