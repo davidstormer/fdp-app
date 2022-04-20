087f443d691babaaf5411838be730a75a33690b5
Tristan Chambers <tmchambers@nacdl.org>
2022-04-20 11:28:17 -0400

    Add various fields to search listing, add teaser template, refine prefetching

--- profiles/templatetags/teasers.py
+++ profiles/templatetags/teasers.py
@@ -7,6 +7,27 @@ register = template.Library()
 
 
 @register.filter
-def officer_teaser(officer: Person) -> str:
-    template = Template("Hello world {{officer_name}}")
-    return template.render(Context({'officer_name': officer.name}))
+def teaser_person(person: Person) -> str:
+    # PREPROCESSING
+    # Aliases
+    aliases = [alias.name for alias in person.person_aliases.all()]
+    # Identifiers
+    identifiers = [identifier.identifier for identifier in person.person_identifiers.all()]
+    # Commands
+    groups = [person_grouping.grouping.name for person_grouping in person.person_groupings.all()]
+
+    context = Context({
+        'name': person.name,
+        'aliases': aliases,
+        'identifiers': identifiers,
+        'groups': groups,
+    })
+
+    # TEMPLATING
+    template_ = Template("""{{name}} 
+    {% if aliases %}({{ aliases|join:', ' }}){% endif %}
+    {% if identifiers %} &ndash; {{ identifiers|join:', ' }}{% endif %}
+    {% if groups %} &ndash; {{ groups|join:', ' }}{% endif %}
+    """)
+
+    return template_.render(context)
