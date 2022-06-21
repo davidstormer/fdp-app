643090aca31bbccda807928ae6621386e3681e4d
Tristan Chambers <tmchambers@nacdl.org>
2022-06-21 16:12:45 -0400

    Fixup

--- importer_narwhal/admin.py
+++ importer_narwhal/admin.py
@@ -1,9 +1,1 @@
 from django.contrib import admin
-from importer_narwhal.models import ImportBatch
-
-
-class ImportBatchAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(ImportBatch, ImportBatchAdmin)
