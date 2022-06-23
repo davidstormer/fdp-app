ca89a437f0ced98baef24f660b017d5771a526fa
Tristan Chambers <tmchambers@nacdl.org>
2022-06-23 12:29:23 -0400

    Rejigger migrations, folding in 0007_auto_20220623_1406 from develop210414

--- core/migrations/0008_trigram_similarity_extension.py
+++ core/migrations/0008_trigram_similarity_extension.py
@@ -5,7 +5,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0006_auto_20220128_2000'),
+        ('core', '0007_auto_20220623_1406'),
     ]
 
     operations = [
