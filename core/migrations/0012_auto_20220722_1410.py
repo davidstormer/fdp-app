7f54aea77ed0718b6873e5c4a215b7b5eb559f52
Tristan Chambers <tmchambers@nacdl.org>
2022-07-22 12:37:13 -0400

    Include AlterField routines in core/migrations/0012...

--- core/migrations/0012_auto_20220722_1410.py
+++ core/migrations/0012_auto_20220722_1410.py
@@ -20,4 +20,18 @@ class Migration(migrations.Migration):
             old_name='is_inactive',
             new_name='ended_unknown_date',
         ),
+        migrations.AlterField(
+            model_name='grouping',
+            name='ended_unknown_date',
+            field=models.BooleanField(default=False,
+                                      help_text="Select if the grouping is no longer active. Instead of deleting a group, mark it as inactive so that all the data relating to the group and the group history remains. This can also be used if you don't know the ceased date for a group but you know that they are no longer active.",
+                                      verbose_name='ended at unknown date'),
+        ),
+        migrations.AlterField(
+            model_name='persongrouping',
+            name='ended_unknown_date',
+            field=models.BooleanField(default=False,
+                                      help_text='Select if the person is no longer associate with group but the end date is unknown',
+                                      verbose_name='ended at unknown end date'),
+        ),
     ]
