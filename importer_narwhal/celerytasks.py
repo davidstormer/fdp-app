c88ba8230ca771cc335eb55f2285e6c026a5a59a
Tristan Chambers <tmchambers@nacdl.org>
2022-11-21 11:17:28 -0500

    Remove experimental do_a_think code

--- importer_narwhal/celerytasks.py
+++ importer_narwhal/celerytasks.py
@@ -15,22 +15,6 @@ celery_app = Celery('tasks', backend='redis://localhost', broker="redis://localh
 
 
 @celery_app.task
-def do_a_think(num_persons: int):
-    # from celery.contrib import rdb
-    # rdb.set_trace()
-    for i in range(num_persons):
-        name = f'Celery generated person {current_task.request.id} {i}'
-        print(name)
-        Person.objects.create(name=name)
-        current_task.update_state(
-            state='PROGRESS',
-            meta={'on_number': i}
-        )
-        sleep(1)
-    print("done")
-
-
-@celery_app.task
 def background_do_dry_run(batch_pk: int):
     batch = ImportBatch.objects.get(pk=batch_pk)
     do_dry_run(batch)
